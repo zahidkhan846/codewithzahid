7c6de6eab5cb86f719fa3bc372d1122335e0b25b
Zahid Khan <zahid.khan846@hotmail.com>
2021-11-28 01:34:08 +0530

    404 update

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,32 @@
-import { Fragment } from "react";
-import { FaTerminal } from "react-icons/fa";
+import { Fragment, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Meta from "../components/Meta/Meta";
 
 function ErrorPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.back();
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <Fragment>
       <Meta title="Code with Zahid | Error" description="Page unavailble!" />
       <div className="flex-center flex-column gap-2">
-        <FaTerminal className="error-icon" />
-        <h1>404-Page Not Availble!</h1>
-        <h3>Let's try using sidebar items for now...</h3>
+        <h1>404 | Page Not Availble!</h1>
+        <h3>
+          Going back to home page...{" "}
+          <Link href="/">
+            <a style={{ color: "lightseagreen" }}>Click Me</a>
+          </Link>{" "}
+          if it's not automatically redirect.
+        </h3>
       </div>
     </Fragment>
   );
